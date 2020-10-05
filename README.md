d0abaf79deb37c6054e97dc023935fd13fa58837
Ioan Lazar <ilazar@Ioans-MacBook-Pro.local>
2020-10-05 19:14:28 +0300

    02

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ Change log
 - Use 'Empty Activity' template
 - Activity, https://developer.android.com/reference/android/app/Activity
 - Views & resources
+
+02 Activity lifecycle
+
+  - Activity state diagram, https://developer.android.com/reference/android/app/Activity
+  - Android logs, https://developer.android.com/studio/command-line/logcat
+  - Orientation changes
+  - Saving & restoring activity state
--- app/src/main/java/ro/ubbcluj/cs/ilazar/myapp1/MainActivity.kt
+++ app/src/main/java/ro/ubbcluj/cs/ilazar/myapp1/MainActivity.kt
@@ -2,10 +2,63 @@ package ro.ubbcluj.cs.ilazar.myapp1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        val COUNTER = "counter"
+    }
+
+    var counter: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        savedInstanceState?.let {
+            counter = it.getInt(COUNTER, 0)
+        }
+        text_counter.text = "$counter"
+        button_inc.setOnClickListener {
+            counter++
+            text_counter.text = "$counter"
+        }
+        Log.i(TAG, "onCreate")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.i(TAG, "onSaveInstanceState")
+        outState.putInt(COUNTER, counter)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy")
     }
 }
